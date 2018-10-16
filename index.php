a99ac5f95f372adc464c736cdf6d4384c7509947
guido0095 <guido0095@gmail.com>
2018-10-16 16:12:20 +0200

    Added simple PHP file that print the table

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$taskin=file_get_contents("task.json");
+
+$obj=json_decode("$taskin", TRUE);
+
+echo "<table style=\"width:100%\">";
+echo "<tr>
+    <th>ID</th>
+    <th>Priority</th>
+    <th>Title</th>
+    <th>Description</th>
+    <th>Maintainer</th>
+    
+  </tr>";
+
+foreach($obj as $tasklist){
+    
+    echo "<tr>";
+    echo "<td>".$tasklist['id']."</td>";
+    echo "<td>".$tasklist['priority']."</td>";
+    echo "<td>".$tasklist['title']."</td>";
+    echo "<td>";
+    echo isset($tasklist['message']) ? $tasklist['message']: "";
+    echo "</td>";
+    echo "<td>".implode(", ", $tasklist['maintainer'])."</td>";
+    // echo "<td>".$tasklist['done']."</td>";
+    echo "</tr>";
+}
+?>
+
+</body>
+</html> 
