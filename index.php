0f3d8082f6a1abb4f98ea086811bd111f75cb0c8
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-10-21 18:01:49 -0400

    Adds styling to the recently added and modified item lists

--- index.php
+++ index.php
@@ -20,151 +20,158 @@
             </div>
 
             <hr/>
-            
-            <div class="task mt-5">
-                <h4 class="text-center">Tasklist</h4>
-                <table class="table" style="width: 70%; margin: 0 auto;">
-                    <tr>
-                        <th>ID</th>
-                        <th>Priority</th>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Maintainer</th>
-                    </tr>
-                    <?php
-                        $taskin = file_get_contents("task.json");
-                        $obj = json_decode("$taskin", TRUE);
 
-                        foreach ($obj as $tasklist) {
-                            if (!$tasklist['done']) {
-                                echo "<tr>";
-                                echo "<td>".$tasklist['id']."</td>";
-                                echo "<td>".$tasklist['priority']."</td>";
-                                echo "<td>".$tasklist['title']."</td>";
-                                echo "<td>";
-                                echo isset($tasklist['message']) ? $tasklist['message']: "";
-                                echo "</td>";
-                                echo "<td>".implode(", ", $tasklist['maintainer'])."</td>";
-                                //echo "<td>".$tasklist['done']."</td>";
-                                echo "</tr>";
+            <div class="task">
+                <h5 class="text-center">Tasklist</h5>
+                <table class="table table-striped " style="width: 70%; margin: 0 auto;">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Priority</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Maintainer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $taskin = file_get_contents("task.json");
+                            $obj = json_decode("$taskin", TRUE);
+
+                            foreach ($obj as $tasklist) {
+                                if (!$tasklist['done']) {
+                                    echo "<tr>";
+                                    echo "<td>".$tasklist['id']."</td>";
+                                    echo "<td>".$tasklist['priority']."</td>";
+                                    echo "<td>".$tasklist['title']."</td>";
+                                    echo "<td>";
+                                    echo isset($tasklist['message']) ? $tasklist['message']: "";
+                                    echo "</td>";
+                                    echo "<td>".implode(", ", $tasklist['maintainer'])."</td>";
+                                    //echo "<td>".$tasklist['done']."</td>";
+                                    echo "</tr>";
+                                }
                             }
-                        }
-                    ?>
+                        ?>
+                    </tbody>
                 </table> 
             </div>
 
             <hr/>
 
-<?php
-echo "<div class=\"statswrapper\">
-<p>Recently added items:</p>
-<table style=\"width:30%\">
-    <thead>
-    <tr>
-        <td>Item</td>
-        <td>Added</td>
-    </tr>
-    </thead>
-    <tbody>
-                    <tr>
-            <td><a href=\"/item/R374\">R374</a></td>
-            <td>2018-10-18, 20:01</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/R373\">R373</a></td>
-            <td>2018-10-18, 20:01</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/C165\">C165</a></td>
-            <td>2018-10-18, 20:01</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/B152\">B152</a></td>
-            <td>2018-10-18, 20:01</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/R372\">R372</a></td>
-            <td>2018-10-18, 13:20</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/R371\">R371</a></td>
-            <td>2018-10-18, 13:20</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/127\">127</a></td>
-            <td>2018-10-16, 19:37</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/126\">126</a></td>
-            <td>2018-10-16, 19:16</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/HDD228\">HDD228</a></td>
-            <td>2018-10-16, 19:07</td>
-        </tr>
-                    <tr>
-            <td><a href=\"/item/HDD227\">HDD227</a></td>
-            <td>2018-10-16, 19:04</td>
-        </tr>
-    </tbody>
-<table>
-</div></tr></td>";
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="statswrapper">
+                        <h6>Recently added items:</h6>
+                        <table class="table table-striped table-sm ">
+                            <thead>
+                                <tr>
+                                    <td>Item</td>
+                                    <td>Added</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a href="/item/R374">R374</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/R373">R373</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/C165">C165</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/B152">B152</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/R372">R372</a></td>
+                                    <td>2018-10-18, 13:20</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/R371">R371</a></td>
+                                    <td>2018-10-18, 13:20</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/127">127</a></td>
+                                    <td>2018-10-16, 19:37</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/126">126</a></td>
+                                    <td>2018-10-16, 19:16</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/HDD228">HDD228</a></td>
+                                    <td>2018-10-16, 19:07</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/HDD227">HDD227</a></td>
+                                    <td>2018-10-16, 19:04</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
 
-echo "<div align=right class=\"statswrapper\">
-<p>Recently modified items:</p>
-<table align=right style=\"width:30%\">
-	<thead>
-	<tr>
-		<td>Item</td>
-		<td>Modified</td>
-	</tr>
-	</thead>
-	<tbody>
-					<tr>
-			<td><a href=\"/item/R374\">R374</a></td>
-			<td>2018-10-18, 20:01</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/R373\">R373</a></td>
-			<td>2018-10-18, 20:01</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/C165\">C165</a></td>
-			<td>2018-10-18, 20:01</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/B152\">B152</a></td>
-			<td>2018-10-18, 20:01</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/R372\">R372</a></td>
-			<td>2018-10-18, 13:20</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/R371\">R371</a></td>
-			<td>2018-10-18, 13:20</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/127\">127</a></td>
-			<td>2018-10-16, 19:37</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/126\">126</a></td>
-			<td>2018-10-16, 19:16</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/HDD228\">HDD228</a></td>
-			<td>2018-10-16, 19:07</td>
-		</tr>
-					<tr>
-			<td><a href=\"/item/HDD227\">HDD227</a></td>
-			<td>2018-10-16, 19:04</td>
-		</tr>
-				</tbody>
-	</table>
-    <div>";
-?>
-
+                <div class="col-md-6">
+                    <div class="statswrapper">
+                        <h6>Recently modified items:</h6>
+                        <table class="table table-striped table-sm ">
+                            <thead>
+                                <tr>
+                                    <td>Item</td>
+                                    <td>Modified</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a href="/item/R374">R374</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/R373">R373</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/C165">C165</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/B152">B152</a></td>
+                                    <td>2018-10-18, 20:01</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/R372">R372</a></td>
+                                    <td>2018-10-18, 13:20</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/R371">R371</a></td>
+                                    <td>2018-10-18, 13:20</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/127">127</a></td>
+                                    <td>2018-10-16, 19:37</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/126">126</a></td>
+                                    <td>2018-10-16, 19:16</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/HDD228">HDD228</a></td>
+                                    <td>2018-10-16, 19:07</td>
+                                </tr>
+                                <tr>
+                                    <td><a href="/item/HDD227">HDD227</a></td>
+                                    <td>2018-10-16, 19:04</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    <div>
+                </div>
+            </div>
         </div>
 
         <script type="text/javascript"> 
